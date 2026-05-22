c81a6bd1ae02b5d1726da68f0a10c30dc6500c57
Dmitry Romashov <framebassman@gmail.com>
2026-05-22 18:58:07 +0200

    Move to aws cli

--- ansible/roles/3x-ui/files/restore.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-apk add --no-cache aws-cli zstd >/dev/null 2>&1
-
-aws s3 cp "s3://backups/${BACKUP_OBJECT_KEY}" /tmp/backup.archive
-
-cd /restore
-zstd -dc /tmp/backup.archive | tar -x --strip-components=2
